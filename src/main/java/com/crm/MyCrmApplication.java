17fe12e03f032aa2edba5f34ce58313b591a3bdf
root <zheyuan1997@126.com>
2021-09-02 11:46:05 +0800

    myCrm third commit

--- src/main/java/com/crm/MyCrmApplication.java
+++ src/main/java/com/crm/MyCrmApplication.java
@@ -9,6 +9,7 @@ public class MyCrmApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyCrmApplication.class, args);
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 
 }
