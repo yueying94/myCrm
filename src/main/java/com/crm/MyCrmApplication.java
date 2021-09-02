49acdc131d02153584fda580005f7c927722320a
root <zheyuan1997@126.com>
2021-09-02 12:07:47 +0800

    myCrm branch commit

--- src/main/java/com/crm/MyCrmApplication.java
+++ src/main/java/com/crm/MyCrmApplication.java
@@ -8,7 +8,7 @@ public class MyCrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyCrmApplication.class, args);
-        System.out.println("hello,git");
+        System.out.println("hello,git,我要改一些东西");
     }
 
 }
