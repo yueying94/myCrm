d7e91248e192e77d2d46913ec1156955de377f4a
root <zheyuan1997@126.com>
2021-09-02 11:42:34 +0800

    myCrm second commit

--- src/main/java/com/crm/MyCrmApplication.java
+++ src/main/java/com/crm/MyCrmApplication.java
@@ -8,6 +8,7 @@ public class MyCrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyCrmApplication.class, args);
+        System.out.println("hello,git");
     }
 
 }
